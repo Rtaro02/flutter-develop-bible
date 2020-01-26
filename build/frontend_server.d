225279e674b58e7f70f9786726ffadb29b779b01
Rtaro02 <nmeeaiwxntwsoetnlelin@gmail.com>
2020-01-26 10:48:19 +0900

    init

---
+++ build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: /Users/ryotaro/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+  import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.pink,
+    ),
+    home: Scaffold(
+      body: Center(
+        child: Text(
+          'Flutter Demo Home Page',
+          style: Theme.of(context).textTheme.display1,
+        ),
+      ),
+    ),
+  );
+}
